b0f1e8cc5c6a440d2def7deb04d8693cef22290f
Angus <angus.moore@internode.on.net>
2017-02-15 07:24:40 +1100

    Reordering of files into sensible folders, plus a start at adding type annotations to function definitions.

--- src/constructor/utils.jl
+++ src/constructor/utils.jl
@@ -30,10 +30,28 @@ function copy(pert::PerturbationSystem)
     return Meta([deepcopy(getfield(pert, k)) for k = 1:length(fieldnames(pert))]...)
 end
 
-function createpairedtuplelist(keyarray,valuearray)
+function createpairedtuplelist(keyarray::Array{Any},valuearray::Array{Any})
     out = ()
     for (key,val) in zip(keyarray,valuearray)
         out = (out...,(key,val))
     end
     return out
+end
+
+function checknames(names::Int)
+    # This function throws an error for names that are unacceptable/reserved in sympy (i.e. pi, inf etc)
+    badnames = Array(String,0)
+    push!(badnames,"Inf")
+    push!(badnames,"inf")
+    push!(badnames,"exp")
+    push!(badnames,"pi")
+    push!(badnames,"log")
+    push!(badnames,"beta")
+
+    for name in names
+        if any(badnames.==name)
+            error("Name: $(name) is a reserved name in SymPy. Please change it.")
+        end
+    end
+    return 
 end
